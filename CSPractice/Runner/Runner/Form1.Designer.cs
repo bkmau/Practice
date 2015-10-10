97c8367d88f1bc7ff93dab9c2609e7196207a09b
bkmau <ft52000f@gmail.com>
2015-10-10 12:04:01 +0800

    add runner

---
+++ CSPractice/Runner/Runner/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Runner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.images = new System.Windows.Forms.ImageList(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // images
+            // 
+            this.images.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("images.ImageStream")));
+            this.images.TransparentColor = System.Drawing.Color.Transparent;
+            this.images.Images.SetKeyName(0, "man1.jpg");
+            this.images.Images.SetKeyName(1, "man2.jpg");
+            this.images.Images.SetKeyName(2, "man3.jpg");
+            this.images.Images.SetKeyName(3, "man4.jpg");
+            // 
+            // timer
+            // 
+            this.timer.Interval = 500;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(57, 27);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(167, 23);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "跑 跑 跑 向前跑~";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(161, 66);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(90, 35);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "開跑";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(161, 121);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(90, 35);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "停下";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(34, 177);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(68, 23);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "label2";
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(38, 66);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(90, 90);
+            this.pic.TabIndex = 4;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 227);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList images;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
