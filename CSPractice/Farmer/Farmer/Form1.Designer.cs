88148c0cfe22380f397c82e63713dfe64dba7eb8
bkmau <ft52000f@gmail.com>
2015-11-01 23:02:43 +0800

    finish farmer

---
+++ CSPractice/Farmer/Farmer/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Farmer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbRight = new System.Windows.Forms.ListBox();
+            this.lbLeft = new System.Windows.Forms.ListBox();
+            this.btnRtoL = new System.Windows.Forms.Button();
+            this.btnLtoR = new System.Windows.Forms.Button();
+            this.btnHow = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbRight
+            // 
+            this.lbRight.FormattingEnabled = true;
+            this.lbRight.ItemHeight = 23;
+            this.lbRight.Location = new System.Drawing.Point(291, 64);
+            this.lbRight.Name = "lbRight";
+            this.lbRight.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbRight.Size = new System.Drawing.Size(120, 142);
+            this.lbRight.TabIndex = 11;
+            // 
+            // lbLeft
+            // 
+            this.lbLeft.FormattingEnabled = true;
+            this.lbLeft.ItemHeight = 23;
+            this.lbLeft.Location = new System.Drawing.Point(21, 64);
+            this.lbLeft.Name = "lbLeft";
+            this.lbLeft.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbLeft.Size = new System.Drawing.Size(120, 142);
+            this.lbLeft.TabIndex = 10;
+            // 
+            // btnRtoL
+            // 
+            this.btnRtoL.Location = new System.Drawing.Point(169, 140);
+            this.btnRtoL.Name = "btnRtoL";
+            this.btnRtoL.Size = new System.Drawing.Size(95, 40);
+            this.btnRtoL.TabIndex = 9;
+            this.btnRtoL.Text = "<------";
+            this.btnRtoL.UseVisualStyleBackColor = true;
+            this.btnRtoL.Click += new System.EventHandler(this.btnRtoL_Click);
+            // 
+            // btnLtoR
+            // 
+            this.btnLtoR.Location = new System.Drawing.Point(169, 84);
+            this.btnLtoR.Name = "btnLtoR";
+            this.btnLtoR.Size = new System.Drawing.Size(95, 40);
+            this.btnLtoR.TabIndex = 8;
+            this.btnLtoR.Text = "------>";
+            this.btnLtoR.UseVisualStyleBackColor = true;
+            this.btnLtoR.Click += new System.EventHandler(this.btnLtoR_Click);
+            // 
+            // btnHow
+            // 
+            this.btnHow.Location = new System.Drawing.Point(231, 17);
+            this.btnHow.Name = "btnHow";
+            this.btnHow.Size = new System.Drawing.Size(98, 33);
+            this.btnHow.TabIndex = 7;
+            this.btnHow.Text = "說明";
+            this.btnHow.UseVisualStyleBackColor = true;
+            this.btnHow.Click += new System.EventHandler(this.btnHow_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(78, 22);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(101, 23);
+            this.lbl1.TabIndex = 6;
+            this.lbl1.Text = "農夫Carry";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 240);
+            this.Controls.Add(this.lbRight);
+            this.Controls.Add(this.lbLeft);
+            this.Controls.Add(this.btnRtoL);
+            this.Controls.Add(this.btnLtoR);
+            this.Controls.Add(this.btnHow);
+            this.Controls.Add(this.lbl1);
+            this.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbRight;
+        private System.Windows.Forms.ListBox lbLeft;
+        private System.Windows.Forms.Button btnRtoL;
+        private System.Windows.Forms.Button btnLtoR;
+        private System.Windows.Forms.Button btnHow;
+        private System.Windows.Forms.Label lbl1;
+    }
+}
+
---
+++ CSPractice/Farmer/Farmer/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farmer
+{
+    public partial class Form1 : Form
+    {
+        int[,] arr;
+        string[] obj = { "農夫", "小羊", "蔬菜", "野狼" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             *          小羊  蔬菜  野狼
+             * 小羊     0      1      1
+             * 蔬菜     1      0      0
+             * 野狼     1      0      0
+             */
+            arr = new int[,] { { 0, 1, 1 }, { 1, 0, 0 }, { 1, 0, 0 } };
+            lbLeft.Items.AddRange(obj);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            HowTo();
+        }
+
+        private void btnHow_Click(object sender, EventArgs e)
+        {
+            HowTo();
+        }
+
+        private void HowTo()
+        {
+            string str = "~~規則~~\r\n" +
+                "1. 農夫一次只能帶小羊、蔬菜或野狼其中一樣或自己過河。\r\n" +
+                "2. 農夫不在時，小羊會吃蔬菜；野狼會吃小羊。\r\n" +
+                "3. 全部都過河就carry成功。\r\n";
+            MessageBox.Show(str);
+        }
+
+        private void btnLtoR_Click(object sender, EventArgs e)
+        {
+            Cross(lbLeft, lbRight);
+            GameOver(lbRight);
+            GameOver(lbLeft);
+        }
+
+        private void btnRtoL_Click(object sender, EventArgs e)
+        {
+            Cross(lbRight, lbLeft);
+            GameOver(lbLeft);
+            GameOver(lbRight);
+        }
+
+        private void Cross(ListBox thisSide, ListBox crossSide)
+        {
+            string[] selected = new string[4];
+            int i = 0;
+            bool isFarmerSelected = false;
+            foreach (string s in thisSide.SelectedItems)
+            {
+                selected[i] = s;
+                i++;
+                if (s.Equals("農夫"))
+                {
+                    isFarmerSelected = true;
+                }
+            }
+            int selectedCount = thisSide.SelectedItems.Count; 
+            if (isFarmerSelected)
+            {
+                if (selectedCount > 2)
+                {
+                    MessageBox.Show("一個農夫只能帶一樣東西過河!!");
+                }
+                else
+                if(selectedCount > 0)
+                {
+                    i = 0;
+                    while (i < 2)
+                    {
+                        if (! (selected[i] == null || selected[i].Equals("")))
+                        {
+                            thisSide.Items.Remove(selected[i]);
+                            crossSide.Items.Add(selected[i]);
+                        }
+                        i++;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("需要農夫才能過河!!");
+            }
+            selected = null;
+            thisSide.ClearSelected();
+        }
+
+        private void GameOver(ListBox listbox)
+        {
+            int[] together = new[] { 0, 0, 0 };
+            bool isFarmerHere = false;
+            int i = 0;
+            foreach (string s in listbox.Items)
+            {
+                if (s.Equals("農夫"))
+                {
+                    isFarmerHere = true;
+                }
+                switch (s)
+                {
+                    case "農夫":
+                        break;
+                    case "小羊":
+                        together[i] = 0;
+                        i++;
+                        break;
+                    case "蔬菜":
+                        together[i] = 1;
+                        i++;
+                        break;
+                    case "野狼":
+                        together[i] = 2;
+                        i++;
+                        break;
+                }
+            }
+
+            if (isFarmerHere)
+            {
+                if (i == 3)
+                {
+                    if (listbox.Name.Equals("lbRight"))
+                    {
+                        MessageBox.Show("成功渡河!!");
+                    }
+                }                   
+            }
+            else
+            {
+                if ((i == 3) || ((i == 2) && (arr[together[0], together[1]] == 1)))
+                {
+                    MessageBox.Show("農夫血本無歸!!");
+                    lbLeft.Items.Clear();
+                    lbRight.Items.Clear();
+                    lbLeft.Items.AddRange(obj);
+                }
+            }
+        }
+
+        
+    }
+}
